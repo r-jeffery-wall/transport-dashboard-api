90597594525c03a64890a7d1276a760721caa011
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-09-28 18:14:26 +0100

    Built script for getting weather data as JSON.

--- api/weather.py
+++ api/weather.py
@@ -1,5 +1,31 @@
-#!/usr/bin/python
 from dataclasses import dataclass
+from dotenv import load_dotenv
+from scipy.constants import convert_temperature
 from math import floor
 import requests
 import json
+import os
+
+# Initialise env variables.
+load_dotenv()
+
+# API Variables
+base_url = "https://api.openweathermap.org/data/2.5/weather"
+api_key = os.environ["OPENWEATHER_API_KEY"]
+
+def get_weather_data_for_latlong(lat, lon):
+    request_url = f"{base_url}?lat={lat}&lon={lon}&appid={api_key}"
+
+    request = requests.get(request_url)
+
+    data = request.json()
+
+    return {
+        "temp": floor(convert_temperature(data["main"]["temp"], 'Kelvin', 'Celsius')),
+        "weather": data["weather"][0]["main"],
+        "weather_icon": data["weather"][0]["icon"]
+    }
+
+# For testing:
+if __name__ == "__main__":
+    print(get_weather_data_for_latlong("51.501349", "-0.141668"))
