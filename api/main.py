6d93c1fc4bdf717421820c6a322fa9e6c4d45072
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-10-06 17:48:30 +0100

    Changed '/' endpoint to display a HTML guide to the API.

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import List
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 
 from api_getters import main_api_getters
@@ -16,7 +17,24 @@ class Settings(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return HTMLResponse("""
+    <h1>Transport Dashboard API</h1>
+    <p>This API generates data for a transport dashboard by pulling data from various transport APIs and generating an SVG file that can be displayed on various displays.</p>
+    <p>There are two endpoints: '/RawData' and '/SVG'. The first pulls data in JSON format that you can do what you like with, the second generates an SVG for display. Both endpoints respond to POST requests with the following request schema: </p>
+    <pre>
+    <code>
+    {
+        "latlong": #The coordinates for the weather display.
+        "stops": [ # A list containing the stops that you want to be displayed on the dashboard (max 4)
+            {
+                "stop_type": #The type of the stop. For available types, see the GitHub Readme.
+                "id": # The ID of the stop to pull, this will depend on the stop type. See the GitHub Readme.
+            }
+        ]
+    }
+    </code>
+    </pre>
+    """)
 
 @app.post("/")
 async def get_data(query_settings: Settings):
