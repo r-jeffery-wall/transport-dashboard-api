0c3809b5008ffa2c4feaeeaee6be400fa985b7a4
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-10-07 18:45:57 +0100

    Added basic layout to draw function.

--- draw/svg_generator.py
+++ draw/svg_generator.py
@@ -6,8 +6,21 @@ def draw_dashboard(JSON_data):
     width, height = JSON_data['width'], JSON_data['height']
     dashboard = draw.Drawing(width, height, origin='top-left')
 
+    # Set up basic layout.
+    # Variables
+    status_line = (0, height * 0.2, width, height * 0.2)
+    weather_divider = (width * 0.55, 0, width * 0.55, height * 0.2)
+    grid_vertical = (width / 2, height * 0.2, width / 2, height)
+    grid_horizontal = (0, height / 2, width, height / 2)
+    date_time_y = height * 0.1
+    # Draw layout
+    dashboard.append(draw.Line(status_line[0], status_line[1], status_line[2], status_line[3], stroke='black', stroke_width=2))
+    dashboard.append(draw.Line(weather_divider[0], weather_divider[1], weather_divider[2], weather_divider[3], stroke='black', stroke_width=2))
+    dashboard.append(draw.Line(grid_vertical[0], grid_vertical[1], grid_vertical[2], grid_vertical[3], stroke='black', stroke_width=2))
+    dashboard.append(draw.Line(grid_horizontal[0], grid_horizontal[1], grid_horizontal[2], grid_horizontal[3], stroke='black', stroke_width=2))
+
     # Print Date and Time
-    dashboard.append(draw.Text(JSON_data['date_time'], 24, 0, 15, text_anchor='start', fill='black'))
+    dashboard.append(draw.Text(JSON_data['date_time'], 24, 5, date_time_y, text_anchor='start', fill='black'))
 
 
     dashboard.save_svg('dashboard.svg')
