10569a2d3a6cdb815f1c9f8ca389d4379c82abe5
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-10-19 11:40:25 +0100

    Modified date_time.py to use timezone.

--- api/date_time.py
+++ api/date_time.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def get_date_time_string():
-    date_time = datetime.now()
+    date_time = datetime.now(ZoneInfo("Europe/London")) # Replace with your timezone as appropriate.
     return date_time.strftime("%A %-d %B %Y  %-H:%M")
 
 
